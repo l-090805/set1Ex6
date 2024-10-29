68a6b1622e5a5ee20da042bbcf0a7c4c2313e401
falau <falau@LENOVO>
2024-10-29 19:59:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace set1Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea primei laturi: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Introduceti lungimea celei de a 2 a laturi: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Introduceti lungimea celei de a 3 a laturi: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+
+                if (a < (b + c) && b < (a + c) && c < (a + b))
+                {
+                    Console.WriteLine("Laturile formeaza un triunghi");
+                }
+                else
+                {
+                    Console.WriteLine("Laturile nu formeaza un triunghi");
+                }
+
+
+
+
+        }
+    }
+}
